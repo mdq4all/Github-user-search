b1ecb90327c6a7942fff2f4a6c3e60a05289d1b9
Damian <mdq4all@gmail.com>
2023-09-07 11:14:01 -0300

    agregado typeof al localstorage

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,12 @@ import { useEffect, useState } from "react";
 import SunIcon from "./SunIcon";
 import Image from "next/image";
 
-const initialThemeState = !localStorage.getItem("theme") ? "light" : localStorage.getItem("theme") as "light" | "dark"
+const initialThemeState = () => {
+  if (typeof window !== "undefined") {
+    if (localStorage.getItem("theme")) return localStorage.getItem("theme") as "light" | "dark"
+     return "light"
+  } else return "light"
+};
 
 export default function Header() {
   const [theme, setTheme] = useState<"light" | "dark">(initialThemeState);
@@ -12,15 +17,14 @@ export default function Header() {
   useEffect(() => {
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", theme)
-      
+      localStorage.setItem("theme", theme);
     } else {
       document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", theme)
+      localStorage.setItem("theme", theme);
     }
-  },[theme])
+  }, [theme]);
 
-  const handleClick = () => setTheme(theme === "light" ? "dark" : "light") 
+  const handleClick = () => setTheme(theme === "light" ? "dark" : "light");
 
   return (
     <header className="flex flex-wrap items-center justify-between w-full mb-7">
